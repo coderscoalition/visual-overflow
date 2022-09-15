721c79074537c692b8c7b3578427241b6875baaa
Shadofer <shadowrlrs@gmail.com>
2022-09-15 12:54:19 +0300

    Add `prettier` formatting, refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,62 @@
+import fetch from 'node-fetch';
 import * as vscode from 'vscode';
-import fetch from 'node-fetch'
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('visual-overflow.search', () => {
-    let picker = vscode.window.createQuickPick();
-    picker.title = "Search";
-    picker.placeholder = "Search Stack Overflow";
-    picker.onDidChangeValue(async s => {
-      picker.busy = true;
-      console.log('a')
-      let res = await fetch('https://api.stackexchange.com/search?' + new URLSearchParams({
-        intitle: s,
-        site: 'stackoverflow'
-      }));
-      console.log('b')
-      let json: any = await res.json();
-      picker.items = json.items.map((q: any) => {return {label: q.title}});
-      picker.busy = false;
-    });
-    picker.onDidAccept(e => {
-      // TODO: render question
-      let panel = vscode.window.createWebviewPanel('visualOverflow', 'TODO', vscode.ViewColumn.Beside, {});
-      panel.webview.html = '<h1>TODO</h1>' + JSON.stringify(e)
-    })
-    picker.show();
-	});
+    let disposable = vscode.commands.registerCommand(
+        'visual-overflow.search',
+        () => {
+            // Picker option
+            let picker = vscode.window.createQuickPick();
+            picker.title = 'Search';
+            picker.placeholder = 'Search Stack Overflow';
 
-	context.subscriptions.push(disposable);
+            // Input value listener
+            picker.onDidChangeValue(async (searchValue) => {
+                // Loading status
+                picker.busy = true;
+
+                // Fetch from remote stack API
+                let res = await fetch(
+                    'https://api.stackexchange.com/search?' +
+                        new URLSearchParams({
+                            intitle: searchValue,
+                            site: 'stackoverflow',
+                        })
+                );
+
+                let json = (await res.json()) as any;
+
+                // Map labels to returned titles
+                picker.items = json.items.map((q: any) => {
+                    return { label: q.title };
+                });
+
+                // Disable loading status
+                picker.busy = false;
+            });
+
+            // Title click action
+            picker.onDidAccept(() => {
+                // TODO: render question
+
+                // New panel options
+                let panel = vscode.window.createWebviewPanel(
+                    'visualOverflow',
+                    'TODO',
+                    vscode.ViewColumn.Beside,
+                    {}
+                );
+
+                // Markdown content
+                panel.webview.html = 'TODO';
+            });
+
+            // Setup picker with given listeners
+            picker.show();
+        }
+    );
+
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
