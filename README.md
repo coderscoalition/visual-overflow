95ebe1aee14df3110923d932604924da83742eba
= <otterlord.dev@gmail.com>
2022-09-15 11:16:49 +0200

    Point of origin

---
+++ README.md
@@ -0,0 +1,4 @@
+<span align=center>
+  <h1>Visual Overflow</h1>
+  <p>Stack Overflow for VSCode! It's that simple</p>
+</span>
---
+++ src/extension.ts
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+import fetch from 'node-fetch'
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('visual-overflow.search', () => {
+    let picker = vscode.window.createQuickPick();
+    picker.title = "Search";
+    picker.placeholder = "Search Stack Overflow";
+    picker.onDidChangeValue(async s => {
+      picker.busy = true;
+      let res = await fetch('https://api.stackexchange.com/search?' + new URLSearchParams({
+        intitle: s,
+        site: 'stackoverflow'
+      }));
+      let json: any = await res.json();
+      picker.items = json.items.map((q: any) => {label: q.title});
+      picker.busy = false;
+    });
+    picker.onDidAccept(e => {
+      // TODO: render question
+      let panel = vscode.window.createWebviewPanel('visualOverflow', 'TODO', vscode.ViewColumn.Beside, {});
+      panel.webview.html = '<h1>TODO</h1>' + JSON.stringify(e)
+    })
+    picker.show();
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
