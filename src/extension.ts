f0396b3ef6b0f2ba9b328f75e432a2f35d9288df
= <otterlord.dev@gmail.com>
2022-09-16 13:17:14 +0200

    Fetch answers

--- src/extension.ts
+++ src/extension.ts
@@ -31,12 +31,14 @@ export function activate(context: vscode.ExtensionContext) {
 			picker.placeholder = 'Search Stack Overflow';
 
 			picker.onDidChangeValue(async (s) => {
+        // TODO: Add timeout to prevent requesting on each input
 				picker.busy = true;
 				let res = await fetch(
 					'https://api.stackexchange.com/search?' +
 						new URLSearchParams({
 							intitle: s,
 							site: 'stackoverflow',
+              filter: 'withbody'
 						})
 				);
 				let json: any = await res.json();
@@ -46,17 +48,26 @@ export function activate(context: vscode.ExtensionContext) {
 				picker.busy = false;
 			});
 
-			picker.onDidAccept(() => {
+			picker.onDidAccept(async () => {
         // See response data for https://api.stackexchange.com/docs/search
 				const question: vscode.QuickPickItem & { q?: any } = picker.selectedItems[0];
-				// TODO: render question
+        // TODO: move panel out to own function that accepts question and answer data
 				let panel = vscode.window.createWebviewPanel(
 					'visualOverflow',
 					decodeText(question.q.title),
 					vscode.ViewColumn.Beside,
 					{}
 				);
-				panel.webview.html = `<h1>${question.q.title}</h1>`;
+        let res = await fetch(
+					`https://api.stackexchange.com/questions/${question.q.question_id}/answers?` +
+						new URLSearchParams({
+							site: 'stackoverflow',
+              filter: 'withbody'
+						})
+				);
+        let json: any = await res.json();
+        // TODO: Add upvote/downvote/score/comments/accepted/dates/usernames
+				panel.webview.html = `<h1>${question.q.title}</h1>${question.q.body}<h2>Answers</h2><hr>` + json.items.map((a: any) => `${a.body}`);
 			});
 			picker.show();
 		}
